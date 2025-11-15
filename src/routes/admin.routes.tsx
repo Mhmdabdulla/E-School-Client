2adcec65f3b5e05bcf43e552219cb5bcc6d5c24d
Mhmdabdulla <96muhammedabdulla@gmail.com>
2025-11-15 09:07:03 +0530

    feat:Admin dashboard done

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,15 +1,19 @@
-import {  Route } from "react-router-dom";
+import {  Navigate, Route } from "react-router-dom";
 import LoginPage from "@/pages/admin/Login";
 import ProtectedRoute from "./protected.routes";
 import { UserRole } from "@/lib/constants/role";
 import DashboardLayout from "@/components/admin/layout/DashboardLayout";
+import Dashboard from "@/pages/admin/Dashboard";
 
 
 export const adminRoutes = (
     <>
        <Route path="/admin/login" element={<LoginPage />} />
        <Route element={<ProtectedRoute role={[UserRole.ADMIN]} />}>
-       <Route path="/admin" element={<DashboardLayout />}></Route>
+       <Route path="/admin" element={<DashboardLayout />}>
+       <Route index element={<Navigate to="/admin/dashboard" replace />} />
+       <Route path="dashboard" element={<Dashboard />} />
+       </Route>
        </Route>
     </>
 )
---
+++ src/services/adminServices.ts
@@ -0,0 +1,17 @@
+import apiClient from "../lib/axios"
+import axios from "axios";
+
+export const getAdminDashboard = async () => {
+    try {
+        const res = await apiClient.get("/admins/dashboard")
+        return res.data
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            throw error.response?.data?.message || error.message || "An unknown error occurred";
+        } else {
+            throw "An unexpected error occurred";
+        }
+    }
+}
+
+  
