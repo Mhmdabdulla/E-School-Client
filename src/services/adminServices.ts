79037d1bd16d36bcf8a0681d652ad7a17ab62558
Mohammed Abdulla C <116338289+Mhmdabdulla@users.noreply.github.com>
2025-12-03 19:31:26 +0530

    feat: change monthly revenue status in admin from hard coded data to real data from api (#11)

--- src/services/adminServices.ts
+++ src/services/adminServices.ts
@@ -14,4 +14,17 @@ export const getAdminDashboard = async () => {
     }
 }
 
+export const fetchMonthlyRevenue = async () => {
+  try {
+        const res = await apiClient.get("/admins/monthly-revenue");
+        return res.data.revenue;   // returns array of 12 months
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            throw error.response?.data?.message || error.message || "An unknown error occurred";
+        } else {
+            throw "An unexpected error occurred";
+        }
+    }
+};
+
   
